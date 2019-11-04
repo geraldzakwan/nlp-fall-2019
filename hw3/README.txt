7ae52d5fb0e601a09260c87568589124f0742b77
geraldzakwan <geraldi.dzakwan@gmail.com>
2019-11-03 22:19:56 -0500

    Done part A, unchecked, maybe debugging later

---
+++ hw3/README.txt
@@ -0,0 +1,3 @@
+Capital words are treated as <UNK>, e.g. 'AND' and 'WILL'
+
+Still default feature -> top three words
--- hw3/extract_training_data.py
+++ hw3/extract_training_data.py
@@ -9,10 +9,10 @@ class State(object):
     def __init__(self, sentence = []):
         self.stack = []
         self.buffer = []
-        if sentence: 
+        if sentence:
             self.buffer = list(reversed(sentence))
-        self.deps = set() 
-    
+        self.deps = set()
+
     def shift(self):
         self.stack.append(self.buffer.pop())
 
@@ -27,7 +27,7 @@ class State(object):
     def __repr__(self):
         return "{},{},{}".format(self.stack, self.buffer, self.deps)
 
-   
+
 
 def apply_sequence(seq, sentence):
     state = State(sentence)
@@ -35,25 +35,25 @@ def apply_sequence(seq, sentence):
         if rel == "shift":
             state.shift()
         elif rel == "left_arc":
-            state.left_arc(label) 
+            state.left_arc(label)
         elif rel == "right_arc":
-            state.right_arc(label) 
-         
+            state.right_arc(label)
+
     return state.deps
-   
+
 class RootDummy(object):
     def __init__(self):
         self.head = None
         self.id = 0
-        self.deprel = None    
+        self.deprel = None
     def __repr__(self):
         return "<ROOT>"
 
-     
+
 def get_training_instances(dep_structure):
 
     deprels = dep_structure.deprels
-    
+
     sorted_nodes = [k for k,v in sorted(deprels.items())]
     state = State(sorted_nodes)
     state.stack.append(0)
@@ -61,15 +61,15 @@ def get_training_instances(dep_structure):
     childcount = defaultdict(int)
     for ident,node in deprels.items():
         childcount[node.head] += 1
- 
+
     seq = []
-    while state.buffer: 
+    while state.buffer:
         if not state.stack:
             seq.append((copy.deepcopy(state),("shift",None)))
             state.shift()
             continue
         if state.stack[-1] == 0:
-            stackword = RootDummy() 
+            stackword = RootDummy()
         else:
             stackword = deprels[state.stack[-1]]
         bufferword = deprels[state.buffer[-1]]
@@ -81,26 +81,26 @@ def get_training_instances(dep_structure):
             childcount[stackword.id]-=1
             seq.append((copy.deepcopy(state),("right_arc",bufferword.deprel)))
             state.right_arc(bufferword.deprel)
-        else: 
+        else:
             seq.append((copy.deepcopy(state),("shift",None)))
             state.shift()
-    return seq   
+    return seq
 
 
 dep_relations = ['tmod', 'vmod', 'csubjpass', 'rcmod', 'ccomp', 'poss', 'parataxis', 'appos', 'dep', 'iobj', 'pobj', 'mwe', 'quantmod', 'acomp', 'number', 'csubj', 'root', 'auxpass', 'prep', 'mark', 'expl', 'cc', 'npadvmod', 'prt', 'nsubj', 'advmod', 'conj', 'advcl', 'punct', 'aux', 'pcomp', 'discourse', 'nsubjpass', 'predet', 'cop', 'possessive', 'nn', 'xcomp', 'preconj', 'num', 'amod', 'dobj', 'neg','dt','det']
 
 
 class FeatureExtractor(object):
-       
+
     def __init__(self, word_vocab_file, pos_vocab_file):
-        self.word_vocab = self.read_vocab(word_vocab_file)        
-        self.pos_vocab = self.read_vocab(pos_vocab_file)        
+        self.word_vocab = self.read_vocab(word_vocab_file)
+        self.pos_vocab = self.read_vocab(pos_vocab_file)
         self.output_labels = self.make_output_labels()
 
     def make_output_labels(self):
         labels = []
         labels.append(('shift',None))
-    
+
         for rel in dep_relations:
             labels.append(("left_arc",rel))
             labels.append(("right_arc",rel))
@@ -108,39 +108,142 @@ class FeatureExtractor(object):
 
     def read_vocab(self,vocab_file):
         vocab = {}
-        for line in vocab_file: 
+        for line in vocab_file:
             word, index_s = line.strip().split()
             index = int(index_s)
             vocab[word] = index
-        return vocab     
+        return vocab
 
     def get_input_representation(self, words, pos, state):
         # TODO: Write this method for Part 2
-        return np.zeros(6)
+        word_indices = []
 
-    def get_output_representation(self, output_pair):  
+        # Take three top words from stack
+        for i in range(1, 4):
+            # Check if we need to pad, i.e. use <NULL>
+            if len(state.stack) < i:
+                # Append index of '<NULL>'
+                word_indices.append(self.word_vocab['<NULL>'])
+            else:
+                # Get the i-th top word from the stack
+                word_idx_from_stack = state.stack[(-1)*i]
+
+                # Get the actual word from the sentence
+                word = words[word_idx_from_stack]
+
+                # Check special case for root
+                if word == None:
+                    # Append index of '<ROOT>'
+                    word_indices.append(self.word_vocab['<ROOT>'])
+                else:
+                    # Check special case for <CD> and <NNP>
+                    if pos[word_idx_from_stack] == 'CD':
+                        # Append index of 'CD'
+                        word_indices.append(self.word_vocab['<CD>'])
+                    elif pos[word_idx_from_stack] == 'NNP':
+                        # Append index of 'NNP'
+                        word_indices.append(self.word_vocab['<NNP>'])
+                    else:
+                        # Check if it exists in vocab:
+                        if word in self.word_vocab:
+                            # Get the word index in vocab
+                            word_idx_from_vocab = self.word_vocab[word]
+
+                            # Append to return array
+                            word_indices.append(word_idx_from_vocab)
+                        else:
+                            # Append index of 'UNK'
+                            word_indices.append(self.word_vocab['<UNK>'])
+
+        # Take next three words from buffer
+        for i in range(1, 4):
+            # Check if we need to pad, i.e. use <NULL>
+            if len(state.buffer) < i:
+                # Append index of '<NULL>'
+                word_indices.append(self.word_vocab['<NULL>'])
+            else:
+                # Get the i-th top word from the stack
+                word_idx_from_buffer = state.buffer[(-1)*i]
+
+                # Get the actual word from the sentence
+                word = words[word_idx_from_buffer]
+
+                # Check special case for root
+                if word == None:
+                    # Append index of '<ROOT>'
+                    word_indices.append(self.word_vocab['<ROOT>'])
+                else:
+                    # Check special case for <CD> and <NNP>
+                    if pos[word_idx_from_buffer] == 'CD':
+                        # Append index of 'CD'
+                        word_indices.append(self.word_vocab['<CD>'])
+                    elif pos[word_idx_from_buffer] == 'NNP':
+                        # Append index of 'NNP'
+                        word_indices.append(self.word_vocab['<NNP>'])
+                    else:
+                        # Check if it exists in vocab:
+                        if word in self.word_vocab:
+                            # Get the word index in vocab
+                            word_idx_from_vocab = self.word_vocab[word]
+
+                            # Append to return array
+                            word_indices.append(word_idx_from_vocab)
+                        else:
+                            # Append index of 'UNK'
+                            word_indices.append(self.word_vocab['<UNK>'])
+
+        return np.array(word_indices, dtype='int')
+
+    def get_output_representation(self, output_pair):
         # TODO: Write this method for Part 2
-        return np.zeros(91)
 
-     
-    
+        # Use the already computed output labels to get unique index
+        # for each transition, 0-90 inclusive
+        transition_pair_index = self.output_labels[output_pair]
+
+        # Build the one hot encoding vector, fill the index with 1, rest are 0
+        one_hot_encoded_transition = np.zeros(len(self.output_labels), dtype='int')
+        one_hot_encoded_transition[transition_pair_index] = 1
+
+        return one_hot_encoded_transition
+        # return keras.utils.to_categorical(1, num_classes=len(self.output_labels), dtype='int')
+
+
 def get_training_matrices(extractor, in_file):
     inputs = []
     outputs = []
-    count = 0 
-    for dtree in conll_reader(in_file): 
+    count = 0
+    for dtree in conll_reader(in_file):
         words = dtree.words()
         pos = dtree.pos()
+
+        # print('WORDS')
+        # print(words)
+        #
+        # print('POS')
+        # print(pos)
+
         for state, output_pair in get_training_instances(dtree):
+            # print('STATE')
+            # print(state)
+            # print('INPUT REPRESENTATION')
+            # print(extractor.get_input_representation(words, pos, state))
+            # print('---------------------------------------------------')
             inputs.append(extractor.get_input_representation(words, pos, state))
+
+            # print('OUTPUT_PAIR')
+            # print(output_pair)
+            # print(extractor.get_output_representation(output_pair))
+            # print('---------------------------------------------------')
             outputs.append(extractor.get_output_representation(output_pair))
+
         if count%100 == 0:
             sys.stdout.write(".")
             sys.stdout.flush()
         count += 1
     sys.stdout.write("\n")
     return np.vstack(inputs),np.vstack(outputs)
-       
+
 
 
 if __name__ == "__main__":
@@ -150,13 +253,13 @@ if __name__ == "__main__":
 
     try:
         word_vocab_f = open(WORD_VOCAB_FILE,'r')
-        pos_vocab_f = open(POS_VOCAB_FILE,'r') 
+        pos_vocab_f = open(POS_VOCAB_FILE,'r')
     except FileNotFoundError:
         print("Could not find vocabulary files {} and {}".format(WORD_VOCAB_FILE, POS_VOCAB_FILE))
-        sys.exit(1) 
+        sys.exit(1)
 
 
-    with open(sys.argv[1],'r') as in_file:   
+    with open(sys.argv[1],'r') as in_file:
 
         extractor = FeatureExtractor(word_vocab_f, pos_vocab_f)
         print("Starting feature extraction... (each . represents 100 sentences)")
@@ -164,5 +267,3 @@ if __name__ == "__main__":
         print("Writing output...")
         np.save(sys.argv[2], inputs)
         np.save(sys.argv[3], outputs)
-
-
