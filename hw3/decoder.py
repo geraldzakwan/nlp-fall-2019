f687bde9cea6fb054ac9c099df7cdfc396d15610
geraldzakwan <geraldi.dzakwan@gmail.com>
2019-11-04 01:00:35 -0500

    DONE TAPI MESTI DEBUGGING, KECIL BGT SKORNYA

--- hw3/decoder.py
+++ hw3/decoder.py
@@ -8,28 +8,90 @@ import keras
 
 from extract_training_data import FeatureExtractor, State
 
-class Parser(object): 
+class Parser(object):
 
     def __init__(self, extractor, modelfile):
         self.model = keras.models.load_model(modelfile)
         self.extractor = extractor
-        
+
         # The following dictionary from indices to output actions will be useful
         self.output_labels = dict([(index, action) for (action, index) in extractor.output_labels.items()])
 
+    def is_action_permitted(self, action, state):
+        if action[0] == 'shift':
+            # Can't shift if buffer only contains one word and stack is not empty
+            if len(state.buffer) < 2 and len(state.stack) > 0:
+                return False
+
+            # Root can't be the target of left arc
+            if len(state.stack) > 0:
+                if state.stack[-1] == self.extractor.word_vocab['<ROOT>']:
+                    return False
+
+        # Can't do left or right arc if the stack is empty
+        if action[0] == 'left_arc' or action[0] == 'right_arc':
+            if len(state.stack) == 0:
+                return False
+
+        return True
+
+    def sort_output(self, softmax_output):
+        # Create list of tuple (action, probability)
+        action_prob_tuples = []
+        for i in range(0, len(softmax_output)):
+            action_prob_tuples.append((self.output_labels[i], softmax_output[i]))
+
+        # print(action_prob_tuples)
+
+        # Sort by probability
+        return sorted(action_prob_tuples, key=lambda x: x[1])
+
     def parse_sentence(self, words, pos):
         state = State(range(1,len(words)))
-        state.stack.append(0)    
+        state.stack.append(0)
 
-        while state.buffer: 
-            pass
-            # TODO: Write the body of this loop for part 4 
+        while state.buffer:
+            # TODO: Write the body of this loop for part 4
+
+            input = self.extractor.get_input_representation(words, pos, state)
+
+            # IMPORTANT: Reshape to (1, 6), 1 is the batch_size
+            input.resize(1, len(input))
+
+            # print('INPUT:')
+            # print(type(input))
+            # print(input.shape)
+            # print(input)
+            softmax_output = self.model.predict(input)
+            # print('OUTPUT:')
+            # print(type(softmax_output))
+            # print(softmax_output.shape)
+            # print(softmax_output)
+
+            # IMPORTANT, since softmax_output is of shape (1, 91),
+            # take its first element
+            sorted_actions = self.sort_output(softmax_output[0])
+
+            # Loop over actions, starting with the highest prob
+            for i in range(0, len(sorted_actions)):
+                action = sorted_actions[i][0]
+
+                # Do action only if it's valid,
+                # otherwise try next possible action
+                if self.is_action_permitted(action, state):
+                    if action[0] == 'shift':
+                        state.shift()
+                    elif action[0] == 'left_arc':
+                        state.left_arc(action[1])
+                    elif action[0] == 'right_arc':
+                        state.right_arc(action[1])
+                    break
 
         result = DependencyStructure()
-        for p,c,r in state.deps: 
+        for p,c,r in state.deps:
             result.add_deprel(DependencyEdge(c,words[c],pos[c],p, r))
-        return result 
-        
+        return result
+
 
 if __name__ == "__main__":
 
@@ -38,19 +100,18 @@ if __name__ == "__main__":
 
     try:
         word_vocab_f = open(WORD_VOCAB_FILE,'r')
-        pos_vocab_f = open(POS_VOCAB_FILE,'r') 
+        pos_vocab_f = open(POS_VOCAB_FILE,'r')
     except FileNotFoundError:
         print("Could not find vocabulary files {} and {}".format(WORD_VOCAB_FILE, POS_VOCAB_FILE))
-        sys.exit(1) 
+        sys.exit(1)
 
     extractor = FeatureExtractor(word_vocab_f, pos_vocab_f)
     parser = Parser(extractor, sys.argv[1])
 
-    with open(sys.argv[2],'r') as in_file: 
+    with open(sys.argv[2],'r') as in_file:
         for dtree in conll_reader(in_file):
             words = dtree.words()
             pos = dtree.pos()
             deps = parser.parse_sentence(words, pos)
             print(deps.print_conll())
             print()
-        
