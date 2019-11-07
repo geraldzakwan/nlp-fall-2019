5b37373ebc545e2d76f1edb6530f252ac8ce0f96
geraldzakwan <geraldi.dzakwan@gmail.com>
2019-11-07 17:24:31 -0500

    Last before ADL exam

--- hw3/train_model.py
+++ hw3/train_model.py
@@ -5,7 +5,7 @@ import keras
 from keras import Sequential
 from keras.layers import Flatten, Embedding, Dense
 
-def build_model(word_types, pos_types, outputs, input_length=6, embedding_dim=32):
+def build_model(word_types, pos_types, outputs, input_length=6, embedding_dim=32, dense_1=100, dense_2=10, learning_rate=0.01):
     # TODO: Write this function for part 3
 
     # Build a keras Sequential model
@@ -21,15 +21,15 @@ def build_model(word_types, pos_types, outputs, input_length=6, embedding_dim=32
     model.add(Flatten())
 
     # Two dense layers with 100 and 10 units respectively for the default configuration
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(10, activation='relu'))
+    model.add(Dense(dense_1, activation='relu'))
+    model.add(Dense(dense_2, activation='relu'))
 
     # Add a dense output layer with 91 (output_labels) units
-    model.add(Dense(output_labels, activation='softmax'))
+    model.add(Dense(outputs, activation='softmax'))
 
     # Compile the model with an optimizer and a loss function
     # Default optimizer is Adam with learning rate = 0.01
-    model.compile(keras.optimizers.Adam(lr=0.01), loss='categorical_crossentropy')
+    model.compile(keras.optimizers.Adam(lr=learning_rate), loss='categorical_crossentropy')
 
     return model
 
@@ -48,7 +48,21 @@ if __name__ == "__main__":
 
     extractor = FeatureExtractor(word_vocab_f, pos_vocab_f)
     print("Compiling model.")
-    model = build_model(len(extractor.word_vocab), len(extractor.pos_vocab), len(extractor.output_labels))
+
+    # To use default model, simply don't supply sys.argv[4]
+    # To use modified model, run with 'modified' argument in the very back, i.e.
+    # python train_model.py data/input_train_mod.npy data/target_train_mod.npy data/model_mod.h5 modified
+    if len(sys.argv) > 4:
+        if sys.argv[4] == 'modified':
+            # Modified hyperparams
+            model = build_model(len(extractor.word_vocab), len(extractor.pos_vocab), len(extractor.output_labels), input_length=6, embedding_dim=16, dense_1=100, dense_2=10, learning_rate=0.01)
+        else:
+            # Default model
+            model = build_model(len(extractor.word_vocab), len(extractor.pos_vocab), len(extractor.output_labels))
+    else:
+        # Default model
+        model = build_model(len(extractor.word_vocab), len(extractor.pos_vocab), len(extractor.output_labels))
+
     inputs = np.load(sys.argv[1])
     outputs = np.load(sys.argv[2])
     print("Done loading data.")
