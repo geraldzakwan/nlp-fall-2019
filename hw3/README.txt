9bd77f5af72ce2f3346c77afb30dfc62aebbb2f5
geraldzakwan <geraldi.dzakwan@gmail.com>
2019-11-06 19:47:24 -0500

    Add README and comment the decoder thoroughly

--- hw3/README.txt
+++ hw3/README.txt
@@ -1,3 +1,30 @@
-Capital words are treated as <UNK>, e.g. 'AND' and 'WILL'
+# Identity
 
-Still default feature -> top three words
+Name: Geraldi Dzakwan
+UNI: gd2551
+
+# Some assumptions/design decisions that I made:
+
+- All words are lowered, e.g. words like 'The', 'AND' and 'WILL' will be
+  converted into 'the', 'and' and 'will'. This is needed because vocabulary
+  contains only lowered words. Thus, we need to lower the words so that
+  they are not treated as unknown ('<UNK>'). If not handled, this issue can
+  decrease the accuracy. In my experiment, it is around 4-5% lower than
+  it should be if we don't lower the words.
+
+- I create two subfunctions to help working on decoding (parsing) the sentence
+  in the Parser class. They are:
+
+  1. is_action_permitted(self, action, state)
+     This function basically returns True if an action, e.g. ('left_arc', 'aux'),
+     don't violate some constraints given the current state. It returns False
+     otherwise. The constraints are well commented on the function. They are
+     all the constraints stated in the problem description.
+
+  2. sort_output(self, softmax_output)
+     This function basically accepts the softmax output from the neural network.
+     Softmax output is a list containing the probability where probability at
+     index i refers to the probability of an action from output_label[i], which
+     is kindly provided by Prof. Benajiba as self.output_labels.
+     The function then creates a list of tuple (action, probability) and return
+     the descendingly sorted array based on the probability value.
--- hw3/decoder.py
+++ hw3/decoder.py
@@ -17,41 +17,40 @@ class Parser(object):
         # The following dictionary from indices to output actions will be useful
         self.output_labels = dict([(index, action) for (action, index) in extractor.output_labels.items()])
 
+    # Explanation for this function is provided in the README.txt
     def is_action_permitted(self, action, state):
+        # Shift can't be done if:
         if action[0] == 'shift':
-            # Can't shift if buffer only contains one word and stack is not empty
+            # 1. No words are in the buffer, although this won't likely happen in any case
+            # 2. Buffer only contains one word and stack is not empty
             if len(state.buffer) < 2 and len(state.stack) > 0:
                 return False
 
-            # Root can't be the target of left arc
+            # 3. Root can't be the target of left arc
             if len(state.stack) > 0:
                 if state.stack[-1] == self.extractor.word_vocab['<ROOT>']:
                     return False
 
-        # Can't do left or right arc if the stack is empty
+        # left_arc or right_arc can't be done if the stack is empty
         if action[0] == 'left_arc' or action[0] == 'right_arc':
             if len(state.stack) == 0:
                 return False
 
+        # Anything else is permitted
         return True
 
+    # Explanation for this function is provided in the README.txt
     def sort_output(self, softmax_output):
         # Create list of tuple (action, probability)
+        # Use self.output_labels list which is kindly provided by Prof. Benajiba
+        # to map index in softmax_output to the action tuple
         action_prob_tuples = []
         for i in range(0, len(softmax_output)):
             action_prob_tuples.append((self.output_labels[i], softmax_output[i]))
 
-        # print(action_prob_tuples)
-
-        # Sort by probability
-
-        # IMPORTANT, SORT DESCENDINGLY WKWKWKWK
-        # IF NOT, GET SOMETHING VERY VERY LOW:
-        # Micro Avg. Labeled Attachment Score: 2.487500310937539e-05
-        # Micro Avg. Unlabeled Attachment Score: 0.25653590706698837
-        #
-        # Macro Avg. Labeled Attachment Score: 1.802719699252995e-05
-        # Macro Avg. Unlabeled Attachment Score: 0.2565859057508941
+        # IMPORTANT: Sort by probability descendingly
+        # Careful to not sort ascendingly, because the accuracy will be very very low
+        # as we always predict the most unprobable one
         return sorted(action_prob_tuples, key=lambda x: x[1], reverse=True)
 
     def parse_sentence(self, words, pos):
@@ -61,38 +60,42 @@ class Parser(object):
         while state.buffer:
             # TODO: Write the body of this loop for part 4
 
+            # Use extractor object to get input representation
             input = self.extractor.get_input_representation(words, pos, state)
 
-            # IMPORTANT: Reshape to (1, 6), 1 is the batch_size
+            # IMPORTANT: Reshape to (1, len(input))
+            # Otherwise, an error will be yielded
+            # 1, in this case, is the batch_size (we feed the input one by one)
             input.resize(1, len(input))
 
-            # print('INPUT:')
-            # print(type(input))
-            # print(input.shape)
-            # print(input)
+            # Get the prediction from the neural net
             softmax_output = self.model.predict(input)
-            # print('OUTPUT:')
-            # print(type(softmax_output))
-            # print(softmax_output.shape)
-            # print(softmax_output)
 
-            # IMPORTANT, since softmax_output is of shape (1, 91),
-            # take its first element
+            # IMPORTANT: softmax_output is a 2d array of shape (1, 91).
+            # Thus, to get list of probabilities, take its first element
+            # Sort the actions using subfunction
             sorted_actions = self.sort_output(softmax_output[0])
 
-            # Loop over actions, starting with the highest prob
+            # Loop over actions, starting with the one with highest probability
             for i in range(0, len(sorted_actions)):
                 action = sorted_actions[i][0]
 
-                # Do action only if it's valid,
-                # otherwise try next possible action
+                # Execute the action only if it's valid given the current state
+                # Otherwise, try the next possible action
+                # Check using subfunction
                 if self.is_action_permitted(action, state):
+                    # Execute the action accordingly
                     if action[0] == 'shift':
                         state.shift()
+                    # For left and right arc, supply the relationship type
                     elif action[0] == 'left_arc':
                         state.left_arc(action[1])
                     elif action[0] == 'right_arc':
                         state.right_arc(action[1])
+
+                    # IMPORTANT: Don't forget to break the loop so that
+                    # only one action is executed. The next action
+                    # will be determined again by the neural net
                     break
 
         result = DependencyStructure()
