413fa4614e658cbd7f8be805c9a8455e1440e514
geraldzakwan <geraldi.dzakwan@gmail.com>
2019-11-11 00:40:36 -0500

    Part 1 DONE

--- hw4/lexsub_main.py
+++ hw4/lexsub_main.py
@@ -3,22 +3,49 @@ import sys
 
 from lexsub_xml import read_lexsub_xml
 
-# suggested imports 
+# suggested imports
 from nltk.corpus import wordnet as wn
 from nltk.corpus import stopwords
 import gensim
 import numpy as np
 
-# Participate in the 4705 lexical substitution competition (optional): NO
-# Alias: [please invent some name]
+# Participate in the 4705 lexical substitution competition (optional): YES
+# Alias: peaky_blinders
 
 def tokenize(s):
-    s = "".join(" " if x in string.punctuation else x for x in s.lower())    
-    return s.split() 
+    s = "".join(" " if x in string.punctuation else x for x in s.lower())
+    return s.split()
 
 def get_candidates(lemma, pos):
     # Part 1
-    possible_synonyms = []
+
+    # Return solution as a set to make sure unique lemmas are returned
+    possible_synonyms = set([])
+
+    # Retrieve all lexemes for the particular lemma and pos
+    lexemes = wn.lemmas(lemma, pos=pos)
+
+    # Iterate over lexemes
+    for lexeme in lexemes:
+        # Get the synset for current lexeme
+        synset = lexeme.synset()
+
+        # Get the lemmas from the synset
+        for candidate_lemma in synset.lemmas():
+            # Retrieve the name from a lemma structure
+            candidate_lemma_name = candidate_lemma.name()
+
+            # Make sure we don't add input lemma as solution
+            if candidate_lemma_name != lemma:
+                # Check if lemma contains multiple words
+                if len(candidate_lemma_name.split('_')) > 1:
+                    # Replace '_' with ' ', e.g. 'turn_around' -> 'turn around'
+                    candidate_lemma_name = candidate_lemma_name.replace('_', ' ')
+
+                # Add to the solution
+                possible_synonyms.add(candidate_lemma_name)
+
+    # Return the set
     return possible_synonyms
 
 def smurf_predictor(context):
@@ -31,17 +58,17 @@ def wn_frequency_predictor(context):
     return None # replace for part 2
 
 def wn_simple_lesk_predictor(context):
-    return None #replace for part 3        
-   
+    return None #replace for part 3
+
 class Word2VecSubst(object):
-        
+
     def __init__(self, filename):
-        self.model = gensim.models.KeyedVectors.load_word2vec_format(filename, binary=True)    
+        self.model = gensim.models.KeyedVectors.load_word2vec_format(filename, binary=True)
 
     def predict_nearest(self,context):
         return None # replace for part 4
 
-    def predict_nearest_with_context(self, context): 
+    def predict_nearest_with_context(self, context):
         return None # replace for part 5
 
 if __name__=="__main__":
@@ -51,7 +78,10 @@ if __name__=="__main__":
     #W2VMODEL_FILENAME = 'GoogleNews-vectors-negative300.bin.gz'
     #predictor = Word2VecSubst(W2VMODEL_FILENAME)
 
-    for context in read_lexsub_xml(sys.argv[1]):
-        #print(context)  # useful for debugging
-        prediction = smurf_predictor(context) 
-        print("{}.{} {} :: {}".format(context.lemma, context.pos, context.cid, prediction))
+    # print(get_candidates('slow', 'a'))
+    # print(len(get_candidates('slow', 'a')))
+
+    # for context in read_lexsub_xml(sys.argv[1]):
+    #     #print(context)  # useful for debugging
+    #     prediction = smurf_predictor(context)
+    #     print("{}.{} {} :: {}".format(context.lemma, context.pos, context.cid, prediction))
